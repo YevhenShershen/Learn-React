c7efcdff271253abb61cfa332738f2fbb063286e
Yevhen <shershen.yevhen@gmai>
2022-09-15 19:49:33 +0200

    use function in html element and math expressions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 //npm start
 
 import "./App.css";
-// создание элементарного компонента Htader
+// создание элементарного компонента Header
 const Header = () => {
   return <h2>Elementary component</h2>;
 };
-const Field =()=>{
-  return <input placeholder="Type here" type="text" />
-}
-function Btn(){
-  return <button>Click</button>
+const Field = () => {
+  return <input placeholder="Type here" type="text" />;
+};
+function Btn() {
+  // const text = 'Log in'
+  const res = () => {
+    return "Log in";
+  };
+  return <button>{res()}</button>;
 }
 function App() {
   return (
     <div className="App">
       <Header />
-      <Field/>
-      <Btn/>
+      <Field />
+      <Btn />
+      <button>{3 + 4}</button>
     </div>
   );
 }
