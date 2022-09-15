aaa1d85481a3c6d0d8facbeb9c86033724a3575a
Yevhen <shershen.yevhen@gmai>
2022-09-15 19:33:52 +0200

    create elementary components Field and Btn in App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import "./App.css";
 const Header = () => {
   return <h2>Elementary component</h2>;
 };
+const Field =()=>{
+  return <input placeholder="Type here" type="text" />
+}
+function Btn(){
+  return <button>Click</button>
+}
 function App() {
   return (
     <div className="App">
       <Header />
+      <Field/>
+      <Btn/>
     </div>
   );
 }
