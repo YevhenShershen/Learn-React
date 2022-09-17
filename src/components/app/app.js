fdc289be2ec8b12147b2f43a517b3103891cc87e
Yevhen <shershen.yevhen@gmai>
2022-09-17 15:00:08 +0200

    iterate an array with data and display them in the component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,11 @@ import AppFilter from "../app-filter/app-filter";
 import Employerslist from "../employers-list/employers-list";
 import Employersaddform from "../employers-add-form/employers-add-form";
 function App() {
+  const data = [
+    { name: "John C.", salary: 800 },
+    { name: "Alex M.", salary: 1300 },
+    { name: "Carl P.", salary: 13300 },
+  ];
   return (
     <div className="app">
       <AppInfo />
@@ -12,8 +17,8 @@ function App() {
         <SearchPanel />
         <AppFilter />
       </div>
-      <Employerslist />
-      <Employersaddform/>
+      <Employerslist data={data} />
+      <Employersaddform />
     </div>
   );
 }
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,12 +1,9 @@
 import Employerslistitem from "../employers-list-item/employers-list-item";
-import './employers-list.css'
-const Employerslist = () => {
-  return (
-    <ul className="app-list list-group">
-      <Employerslistitem name="John C." salary={800}/>
-      <Employerslistitem name="Alex M." salary={1300}/>
-      <Employerslistitem name="Carl P." salary={3300}/>
-    </ul>
-  );
+import "./employers-list.css";
+const Employerslist = ({ data }) => {
+  const elements = data.map((item) => {
+    return <Employerslistitem name={item.name} salary={item.salary} />;
+  });
+  return <ul className="app-list list-group">{elements}</ul>;
 };
 export default Employerslist;
