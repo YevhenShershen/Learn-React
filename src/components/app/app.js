d1337cf114532eefa9e3870e75d85dd95f083938
Yevhen <shershen.yevhen@gmai>
2022-09-19 20:36:34 +0200

    первый метод удаления с массива (иммутабельность)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,46 @@
-import "./app.css";
+import { Component } from "react";
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import Employerslist from "../employers-list/employers-list";
 import Employersaddform from "../employers-add-form/employers-add-form";
 import StateComponent from "../learn-react/state-component";
-function App() {
-  const data = [
-    { name: "John C.", salary: 800, increase: true, id: 1 },
-    { name: "Alex M.", salary: 1300, increase: false, id: 2 },
-    { name: "Carl P.", salary: 13300, increase: true, id: 3 },
-  ];
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+import "./app.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "John C.", salary: 800, increase: true, id: 1 },
+        { name: "Alex M.", salary: 1300, increase: false, id: 2 },
+        { name: "Carl P.", salary: 13300, increase: true, id: 3 },
+      ],
+    };
+  }
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+      const before = data.slice(0, index);
+      const after = data.slice(index + 1);
+      const newArr = [...before, ...after];
+      return { data: newArr };
+    });
+  };
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <Employerslist data={this.state.data} onDelete={this.deleteItem} />
+        <Employersaddform />
+        {/* <StateComponent name="Olex" surname="Kewals" link="Google" />
+        <StateComponent name="Olex2" surname="Kewals2" link="Google2" /> */}
       </div>
-      <Employerslist data={data} onDelete={(id) => console.log(id)} />
-      <Employersaddform />
-      <StateComponent name="Olex" surname="Kewals" link="Google" />
-      <StateComponent name="Olex2" surname="Kewals2" link="Google2" />
-    </div>
-  );
+    );
+  }
 }
 export default App;
 //render() метод рендер отвечает за вывод чего то на страницу
