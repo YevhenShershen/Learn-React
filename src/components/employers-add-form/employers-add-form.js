1b80fcb2d44d468b292308286a07da85af3b7daa
Yevhen <shershen.yevhen@gmai>
2022-09-19 20:02:50 +0200

    реализация двойного связыванния в реакте

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -1,24 +1,46 @@
-import './employers-add-form.css'
-const Employersaddform = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          placeholder="Как его зовут?"
-          className="form-control new-post-label"
-        />
-        <input
-          type="number"
-          placeholder="З/П в $?"
-          className="form-control new-post-label"
-        />
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+import { Component } from "react";
+import "./employers-add-form.css";
+class Employersaddform extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: 0,
+    };
+  }
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  render() {
+    const {name, salary} = this.state
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            placeholder="Как его зовут?"
+            className="form-control new-post-label"
+            value={name}
+            name="name"
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            placeholder="З/П в $?"
+            className="form-control new-post-label"
+            value={salary}
+            name="salary"
+            onChange={this.onValueChange}
+          />
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 export default Employersaddform;
