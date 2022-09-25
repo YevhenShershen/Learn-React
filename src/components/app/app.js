b0831fe2675f875e4f96665b38c4148613177515
Yevhen <shershen.yevhen@gmai>
2022-09-25 09:14:43 +0200

    filter (vid: 138)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,6 +16,7 @@ class App extends Component {
         { name: "Carl P.", salary: 13300, increase: true, rise: false, id: 3 },
       ],
       term: "",
+      filter: "rise",
     };
     this.maxId = 4;
   }
@@ -65,17 +66,33 @@ class App extends Component {
   onUpdateSerach = (term) => {
     this.setState({ term: term });
   };
+  //принимает два аргмента
+  //1) массив по которому будем фильтровать
+  //2) аргумент будет по которому будем фильтровать
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case "rise":
+        return items.filter((item) => item.rise);
+      case "moreThen1000":
+        return items.filter((item) => item.salary > 1000);
+      default:
+        return items;
+    }
+  };
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
   render() {
-    const { data, term } = this.state;
+    const { data, term, filter } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
-    const visibleDate = this.searchEmp(data, term);
+    const visibleDate = this.filterPost(this.searchEmp(data, term), filter);
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
         <div className="search-panel">
           <SearchPanel onUpdateSerach={this.onUpdateSerach} />
-          <AppFilter />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
         <Employerslist
           data={visibleDate}
