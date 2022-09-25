57e2a00ca5d6984c450718f7d1861964d82bc204
Yevhen <shershen.yevhen@gmai>
2022-09-25 10:20:50 +0200

    inheritance styles(наследование стилей) in styled-component (vid 143)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./components/app/app";
 import { Button } from "./components/learn-react/state-component";
+import styled from "styled-components";
 //StrictMode это инструмент для обнаружения потенциальных проблем в нашем приложении(говоря простыми словами)
+
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+`;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
-    <Button></Button>
+    <BigButton>+++</BigButton>
   </StrictMode>
 );
