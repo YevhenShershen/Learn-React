4b50929cd6f7c1c36e58c05fb2aee545dcd64c16
Yevhen <shershen.yevhen@gmai>
2022-10-02 20:36:23 +0200

    ref часть вторая (vid 159)

--- src/components/learn-react/Form.js
+++ src/components/learn-react/Form.js
@@ -1,33 +1,56 @@
-import React, {Component} from 'react';
-import {Container} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        //myRef теперь у нас хроаниться ссылка на элемент input которая находится в ДОМ дереве
-        this.myRef = React.createRef();
-    }
-//хук вызывается после render()
-componentDidMount(){
-    this.myRef.current.focus()
-}
+  constructor(props) {
+    super(props);
+    //myRef теперь у нас хроаниться ссылка на элемент input которая находится в ДОМ дереве
+    this.myRef = React.createRef();
+  }
+  //хук вызывается после render()
+  componentDidMount() {
+    this.myRef.current.doSmth();
+  }
 
-    render() {
-        return (
-            <Container>
-                <form className="w-50 border mt-5 p-3 m-auto">
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input ref={this.myRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                </form>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <form className="w-50 border mt-5 p-3 m-auto">
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">
+              Email address
+            </label>
+            <TextInput ref={this.myRef} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">
+              Example textarea
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+        </form>
+      </Container>
+    );
+  }
+}
+class TextInput extends Component {
+  doSmth = () => {
+    console.log("123");
+  };
+  render() {
+    return (
+      <input
+        type="email"
+        className="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    );
+  }
 }
 export default Form;
 //ref это ссылка на элемент или компонент в ДОМ дереве уже в отрисованном интерфейсе на странице(в браузере)
